afcdf62f79ce7e1ae45f88772f74172c169a3feb
hugo-a-garcia <hugo.a.garcia@gmail.com>
2020-09-16 22:16:49 +0200

    Clean-up before adding ultrasound message handler code.

--- src/main.c
+++ src/main.c
@@ -1,65 +1,24 @@
-/// This is a trivial demonstration of the runtime subject-ID assignment logic via the standard register interface.
-/// This node implements a server for uavcan.register.Access.1 and two publishers: the heartbeat publisher using its
-/// fixed subject-ID, and a custom subject of type uavcan.primitive.String.1 via a runtime-assigned ID.
-/// DSDL auto-generation is not used in this example -- everything is (de-)serialized manually.
-///
-/// This node is not PnP -- the node-ID has to be assigned manually when starting the application along with the
-/// SocketCAN interface name. A PnP allocator can be implemented trivially in just a few lines of code but I decided
-/// that it would distract the reader from the main demonstrated behavior, which is runtime subject-ID assignment.
-///
-/// This software is released under Creative Commons CC0.
-/// Author: Pavel Kirienko <pavel.kirienko@zubax.com>
-
-#include <socketcan.h>
-#include <canard.h>
+#include "socketcan.h"
+#include "canard.h"
 #include <string.h>
 #include <time.h>
 #include <stdio.h>
 
-static const uint16_t HeartbeatSubjectID      = 32085;
-//* static const uint16_t RegisterAccessServiceID = 384;
+static const uint16_t HeartbeatSubjectID = 32085;
 
-/// Should match the wildcard "uavcan.pub.*" for publishers. Likewise, "uavcan.sub.*" for subscribers.
-//* static const char MeasurementSubjectIDRegisterName[] = "uavcan.pub.measurement";
-
-//* static uint16_t g_measurement_subject_id = UINT16_MAX;
-
-static void* canardAllocate(CanardInstance* const ins, const size_t amount)
+static void *canardAllocate(CanardInstance *const ins, const size_t amount)
 {
-    (void) ins;
+    (void)ins;
     return malloc(amount);
 }
 
-static void canardFree(CanardInstance* const ins, void* const pointer)
+static void canardFree(CanardInstance *const ins, void *const pointer)
 {
-    (void) ins;
+    (void)ins;
     free(pointer);
 }
 
-// static void publishMeasurement(CanardInstance* const canard)
-// {
-//     // Do not publish messages until the subject-ID is configured.
-//     // A real node would store the value in a non-volatile memory; we do not emulate this behavior in this demo.
-//     if (g_measurement_subject_id <= CANARD_SUBJECT_ID_MAX)
-//     {
-//         static CanardTransferID transfer_id;
-//         const CanardTransfer    transfer = {
-//             .priority       = CanardPriorityNominal,
-//             .transfer_kind  = CanardTransferKindMessage,
-//             .port_id        = g_measurement_subject_id,
-//             .remote_node_id = CANARD_NODE_ID_UNSET,
-//             .transfer_id    = transfer_id,
-//             .payload_size   = 13,
-//             .payload        = "\x0B\x00"
-//                        "So it goes.",
-//         };
-//         ++transfer_id;
-
-//         (void) canardTxPush(canard, &transfer);
-//     }
-// }
-
-static void publishHeartbeat(CanardInstance* const canard, const uint32_t uptime)
+static void publishHeartbeat(CanardInstance *const canard, const uint32_t uptime)
 {
     static CanardTransferID transfer_id;
     const uint8_t payload[7] = {
@@ -72,70 +31,24 @@ static void publishHeartbeat(CanardInstance* const canard, const uint32_t uptime
         0,
     };
     const CanardTransfer transfer = {
-        .priority       = CanardPriorityNominal,
-        .transfer_kind  = CanardTransferKindMessage,
-        .port_id        = HeartbeatSubjectID,
+        .priority = CanardPriorityNominal,
+        .transfer_kind = CanardTransferKindMessage,
+        .port_id = HeartbeatSubjectID,
         .remote_node_id = CANARD_NODE_ID_UNSET,
-        .transfer_id    = transfer_id,
-        .payload_size   = sizeof(payload),
-        .payload        = &payload[0],
+        .transfer_id = transfer_id,
+        .payload_size = sizeof(payload),
+        .payload = &payload[0],
     };
     ++transfer_id;
-    (void) canardTxPush(canard, &transfer);
+    (void)canardTxPush(canard, &transfer);
 }
 
-// static void handleRegisterAccess(CanardInstance* const canard, const CanardTransfer* const request_transfer)
-// {
-//     // The following few lines is a basic hand-crafted special-purpose deserializer.
-//     const uint8_t* pld      = request_transfer->payload;
-//     size_t         pld_size = request_transfer->payload_size;
-//     const uint8_t  name_len = *pld++;
-//     pld_size--;
-//     const uint8_t* const name = pld;
-//     if (name_len >= pld_size)
-//     {
-//         return;  // Bad serialization
-//     }
-//     pld += name_len;
-//     pld_size -= name_len;
-//     const uint8_t value_tag = *pld++;
-//     pld_size--;
-//     uint8_t response[313] = {0};  // All-zero response encodes an empty Value, which represents an invalid register.
-//     size_t  response_size = 9;    // timestamp [7] + flags [1] + value tag [1] + empty value [0]
+static void handleHeartbeat()
+{
+    printf("%s \n", "Boom boom");
+}
 
-//     // Match the name against existing registers.
-//     // If none match, respond with an empty value, thereby indicating the lack of such register.
-//     if ((name_len == sizeof(MeasurementSubjectIDRegisterName) - 1) &&
-//         (0 == memcmp(MeasurementSubjectIDRegisterName, name, name_len)))
-//     {
-//         // Write the register (if the type matches).
-//         if ((value_tag == 10) && (pld_size >= 3) && (*pld == 1))  // Natural16[1]
-//         {
-//             ++pld;
-//             uint16_t value = *pld++;
-//             value |= *pld++ << 8;
-//             pld_size -= 3;
-//             g_measurement_subject_id = value;
-//         }
-//         // Then read it and return the result.
-//         response[7]   = 0b00000001;  // Flags: mutable, not persistent (although normally it should be persistent)
-//         response[8]   = 10;          // Type: Natural16
-//         response[9]   = 1;           // One element
-//         response[10]  = (uint8_t)(g_measurement_subject_id);
-//         response[11]  = (uint8_t)(g_measurement_subject_id >> 8);
-//         response_size = 12;
-//     }
-
-//     // Send the response back. Make sure to re-use the same priority and transfer-ID.
-//     CanardTransfer response_transfer = *request_transfer;
-//     response_transfer.transfer_kind  = CanardTransferKindResponse;
-//     response_transfer.payload_size   = response_size;
-//     response_transfer.payload        = response;
-
-//     (void) canardTxPush(canard, &response_transfer);
-// }
-
-int main(const int argc, const char* const argv[])
+int main(const int argc, const char *const argv[])
 {
     if (argc != 3)
     {
@@ -146,8 +59,8 @@ int main(const int argc, const char* const argv[])
 
     // Initialize the node with a static node-ID as specified in the command-line arguments.
     CanardInstance canard = canardInit(&canardAllocate, &canardFree);
-    canard.mtu_bytes      = CANARD_MTU_CAN_CLASSIC;  // Do not use CAN FD to enhance compatibility.
-    canard.node_id        = (CanardNodeID) atoi(argv[2]);
+    canard.mtu_bytes = CANARD_MTU_CAN_CLASSIC; // Do not use CAN FD to enhance compatibility.
+    canard.node_id = (CanardNodeID)atoi(argv[2]);
 
     // Initialize a SocketCAN socket. Do not use CAN FD to enhance compatibility.
     const SocketCANFD sock = socketcanOpen(argv[1], false);
@@ -157,18 +70,17 @@ int main(const int argc, const char* const argv[])
         return 1;
     }
 
-    // // Configure the library to listen for register access service requests.
-    // CanardRxSubscription srv_register_access;
-    // (void) canardRxSubscribe(&canard,
-    //                          CanardTransferKindRequest,
-    //                          RegisterAccessServiceID,
-    //                          1024U,  // Larger buffers are OK.
-    //                          CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
-    //                          &srv_register_access);
+    CanardRxSubscription heartbeat_subscription;
+    (void)canardRxSubscribe(&canard, // Subscribe to messages uavcan.node.Heartbeat.
+                            CanardTransferKindMessage,
+                            HeartbeatSubjectID, // The fixed Subject-ID of the Heartbeat message type (see DSDL definition).
+                            7,                  // The maximum payload size (max DSDL object size) from the DSDL definition.
+                            CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+                            &heartbeat_subscription);
 
     // The main loop: publish messages and process service requests.
-    const time_t boot_ts     = time(NULL);
-    time_t       next_1hz_at = boot_ts;
+    const time_t boot_ts = time(NULL);
+    time_t next_1hz_at = boot_ts;
     while (true)
     {
         if (next_1hz_at < time(NULL))
@@ -179,30 +91,34 @@ int main(const int argc, const char* const argv[])
         }
 
         // Transmit pending frames.
-        const CanardFrame* txf = canardTxPeek(&canard);
+        const CanardFrame *txf = canardTxPeek(&canard);
         while (txf != NULL)
         {
-            (void) socketcanPush(sock, txf, 0);  // Error handling not implemented
+            (void)socketcanPush(sock, txf, 0); // Error handling not implemented
             canardTxPop(&canard);
-            free((void*) txf);
+            free((void *)txf);
             txf = canardTxPeek(&canard);
         }
 
-        // // Process received frames, if any.
-        // CanardFrame rxf;
-        // uint8_t     buffer[64];
-        // while (socketcanPop(sock, &rxf, sizeof(buffer), buffer, 1000) > 0)  // Error handling not implemented
-        // {
-        //     CanardTransfer transfer;
-        //     if (canardRxAccept(&canard, &rxf, 0, &transfer))
-        //     {
-        //         if ((transfer.transfer_kind == CanardTransferKindRequest) &&
-        //             (transfer.port_id == RegisterAccessServiceID))
-        //         {
-        //             handleRegisterAccess(&canard, &transfer);
-        //         }
-        //         free((void*) transfer.payload);
-        //     }
-        // }
+        // Process received frames, if any.
+        CanardFrame rxf;
+        uint8_t buffer[64];
+        while (socketcanPop(sock, &rxf, sizeof(buffer), buffer, 1000) > 0) // Error handling not implemented
+        {
+            CanardTransfer transfer;
+            if (canardRxAccept(&canard, &rxf, 0, &transfer))
+            {
+                if ((transfer.transfer_kind == CanardTransferKindMessage) &&
+                    (transfer.port_id == HeartbeatSubjectID))
+                {
+                    //handleRegisterAccess(&canard, &transfer);
+                    handleHeartbeat();
+                }
+
+                // ADD ULTRASOUND MESSAGE HANDLER HERE //
+
+                free((void *)transfer.payload);
+            }
+        }
     }
 }
